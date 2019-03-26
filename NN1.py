9d164c39635f6407a0a97f80eae7ce977b81dc9c
“Ginocola” <lunli.blue@gmail.com>
2019-03-26 14:40:21 -0400

    try

--- NN1.py
+++ NN1.py
@@ -11,13 +11,14 @@ y_data = x_data*0.1+0.3
 Weights = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 biases = tf.Variable(tf.zeros([1]))
 
-y = Weights*x_data +biases
+y = Weights*x_data + biases
 
 loss = tf.reduce_mean(tf.square(y-y_data))
 optimizer = tf.train.GradientDescentOptimizer(0.5)
 train = optimizer.minimize(loss)
 
 init = tf.initialize_all_variables()
+
 # create tensorflow structure end ###
 
 sess = tf.Session()
@@ -25,6 +26,6 @@ sess.run(init)
 
 for step in range(201):
     sess.run(train)
-    if step%20 == 0:
+    if step % 20 == 0:
         print(step, sess.run(Weights), sess.run(biases))
 
