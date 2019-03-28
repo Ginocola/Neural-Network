7e41648362f3aca08595556c50f75a790997fbaf
“Ginocola” <lunli.blue@gmail.com>
2019-03-28 17:01:16 -0400

    Tensorboard 2

--- NN1.py
+++ NN1.py
@@ -3,19 +3,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def add_layer(inputs, in_size, out_size, activation_function=None):
-    with tf.name_scope('layer'):
-        with tf.name_scope('Weights'):
-            Weights = tf.Variable(tf.random_normal([in_size, out_size]), name='W')
-        with tf.name_scope('biases'):
-            biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, name='b')
-        with tf.name_scope('Wx_plus'):
-            Wx_plus_b = tf.matmul(inputs, Weights) + biases
+def add_layer(inputs, in_size, out_size, n_layer, activation_function=None):
+    # add one more layer and return the output of this layer
+    layer_name = 'layer%s' % n_layer
+    with tf.name_scope(layer_name):
+        # with tf.name_scope('weights'):
+        #     Weights = tf.Variable(tf.random_normal([in_size, out_size]), name='W')
+        #     tf.summary.histogram(layer_name + '/weights', Weights)
+        # with tf.name_scope('biases'):
+        #     biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, name='b')
+        #     tf.summary.histogram(layer_name + '/biases', biases)
+        Weights = tf.Variable(tf.random_normal([in_size, out_size]), name='W')
+        tf.summary.histogram('/weights', Weights)
+        biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, name='b')
+        tf.summary.histogram('/biases', biases)
+        with tf.name_scope('Wx_plus_b'):
+            Wx_plus_b = tf.add(tf.matmul(inputs, Weights), biases)
         if activation_function is None:
             outputs = Wx_plus_b
         else:
-            outputs = activation_function(Wx_plus_b)
-        return outputs
+            outputs = activation_function(Wx_plus_b, )
+        tf.summary.histogram('/outputs', outputs)
+    return outputs
 
 
 # Make up some real data
@@ -23,29 +32,30 @@ x_data = np.linspace(-1, 1, 300)[:, np.newaxis]
 noise = np.random.normal(0, 0.05, x_data.shape)
 y_data = np.square(x_data) - 0.5 + noise
 
-##plt.scatter(x_data, y_data)
-##plt.show()
-
 # define placeholder for inputs to network
 with tf.name_scope('inputs'):
     xs = tf.placeholder(tf.float32, [None, 1], name='x_input')
     ys = tf.placeholder(tf.float32, [None, 1], name='y_input')
 
 # add hidden layer
-l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
+l1 = add_layer(xs, 1, 10, n_layer=1, activation_function=tf.nn.relu)
 # add output layer
-prediction = add_layer(l1, 10, 1, activation_function=None)
+prediction = add_layer(l1, 10, 1, n_layer=2, activation_function=None)
 
-# the error between prediction and real data
+# the error between prediciton and real data
 with tf.name_scope('loss'):
-    loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys-prediction), reduction_indices=[1]))
+    loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
+                                        reduction_indices=[1]))
+    tf.summary.scalar('loss', loss)
+
 with tf.name_scope('train'):
     train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
 sess = tf.Session()
+merged = tf.summary.merge_all()
+
 writer = tf.summary.FileWriter("logs/", sess.graph)
 
-# important step
 init = tf.global_variables_initializer()
 sess.run(init)
 
@@ -56,11 +66,12 @@ ax.scatter(x_data, y_data)
 plt.ion()
 plt.show()
 
-
 for i in range(1000):
-    # training
     sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
     if i % 50 == 0:
+        result = sess.run(merged,
+                          feed_dict={xs: x_data, ys: y_data})
+        writer.add_summary(result, i)
         # to visualize the result and improvement
         try:
             ax.lines.remove(lines[0])
@@ -70,4 +81,3 @@ for i in range(1000):
         # plot the prediction
         lines = ax.plot(x_data, prediction_value, 'r-', lw=5)
         plt.pause(1)
-
