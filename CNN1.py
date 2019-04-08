6e068409fd44925e17a862dc08763f32faa5681c
“Ginocola” <lunli.blue@gmail.com>
2019-04-08 15:35:40 -0400

    Convolutional neural network

--- CNN1.py
+++ CNN1.py
@@ -31,11 +31,11 @@ def conv2d(x, W):
 
 def max_pool_2x2(x):
     # stride [1, x_movement, y_movement, 1]
-    return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 
 # define placeholder for inputs to network
-xs = tf.placeholder(tf.float32, [None, 784])/255.   # 28x28
+xs = tf.placeholder(tf.float32, [None, 784])   # 28x28
 ys = tf.placeholder(tf.float32, [None, 10])
 keep_prob = tf.placeholder(tf.float32)
 x_image = tf.reshape(xs, [-1, 28, 28, 1])
@@ -43,16 +43,16 @@ x_image = tf.reshape(xs, [-1, 28, 28, 1])
 
 
 ## conv1 layer ##
-W_conv1 = weight_variable([5,5, 1,32]) # patch 5x5, in size 1, out size 32
+W_conv1 = weight_variable([5, 5, 1, 32])  # patch 5x5, in size 1, out size 32
 b_conv1 = bias_variable([32])
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1) # output size 28x28x32
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)  # output size 28x28x32
 h_pool1 = max_pool_2x2(h_conv1)                                         # output size 14x14x32
 
 
 ## conv2 layer ##
-W_conv2 = weight_variable([5,5, 32, 64]) # patch 5x5, in size 32, out size 64
+W_conv2 = weight_variable([5, 5, 32, 64])  # patch 5x5, in size 32, out size 64
 b_conv2 = bias_variable([64])
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2) # output size 14x14x64
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)  # output size 14x14x64
 h_pool2 = max_pool_2x2(h_conv2)                                         # output size 7x7x64
 
 
